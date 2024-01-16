f1ab181d78c25db522adc80cf515bb947430a872
Citral87 <thierry.salzedas@gmail.com>
2024-01-16 12:14:12 +0100

    ajout label dans forms

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faPhone, faMapMarkerAlt, faEnvelope,} from "@fortawesome/free-solid-svg-icons";
+import {
+  faPhone,
+  faMapMarkerAlt,
+  faEnvelope,
+} from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 import "../../src/components/css/ContactForm.css";
 
@@ -112,6 +116,7 @@ function ContactForm() {
               value={formData.email}
               onChange={handleChange}
             />
+            <label htmlFor="message">Votre message:</label>
             <textarea
               id="message"
               name="message"
